832610d580411cd4e23bea90821721bd3ffb204b
kim <82611452+Kimdsle@users.noreply.github.com>
2024-07-08 08:31:16 +0700

    Update EnvironmentCheck.java

--- EnvironmentCheck.java
+++ EnvironmentCheck.java
@@ -1,17 +1,23 @@
 package com.jewelry.KiraJewelry;
 
+import javax.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-import javax.annotation.PostConstruct;
 
 @Component
 public class EnvironmentCheck {
+
     private static final Logger logger = LoggerFactory.getLogger(EnvironmentCheck.class);
 
     @PostConstruct
     public void init() {
-        String credentialsPath = System.getenv("GOOGLE_APPLICATION_CREDENTIALS");
-        logger.info("GOOGLE_APPLICATION_CREDENTIALS: " + credentialsPath);
+        logger.info("EnvironmentCheck component initialized");
+        String googleCreds = System.getenv("GOOGLE_APPLICATION_CREDENTIALS");
+        if (googleCreds != null) {
+            logger.info("GOOGLE_APPLICATION_CREDENTIALS: " + googleCreds);
+        } else {
+            logger.error("GOOGLE_APPLICATION_CREDENTIALS environment variable is not set.");
+        }
     }
 }
